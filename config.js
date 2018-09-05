481f757828be3b74cb573834ca2c7cdafb5a49ff
Joshua Drumm <joshing_you1@yahoo.com>
2018-09-05 09:44:19 -0400

    Added routers for each endpoint and server functions

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/nephrology-nexus';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-nephrology-nexus';
--- server.js
+++ server.js
@@ -1,9 +1,62 @@
 "use strict";
 
 const express = require('express');
+const morgan = require("morgan");
+
 const app = express();
 
-app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+const patientsRouter = require("./patientsRouter");
+const clinicsRouter = require("./clinicsRouter");
+const labResultsRouter = require("./labResultsRouter");
 
-module.exports = app;
+app.use(express.static('public'));
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use("/patients", patientsRouter);
+app.use("/clinics", clinicsRouter);
+app.use("/lab-results", labResultsRouter);
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+    return new Promise(function(resolve, reject) {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+                .on("error", err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect()
+        .then(() => {
+            return new Promise((resolve, reject) => {
+                console.log("Closing the server");
+                server.close(err => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve();
+                });
+            });
+        });
+}
+
+if (require.main === module) {
+    runServer(DATABASE_URL).catch(err => console.log(err));
+}
+
+module.exports = {app, runServer, closeServer};
