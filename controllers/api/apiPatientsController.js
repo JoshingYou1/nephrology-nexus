413bbae5b763b6ebf668b5a4029b7761e5943611
Joshua Drumm <joshing_you1@yahoo.com>
2018-12-26 19:44:39 -0500

    Added endpoint to apiPatientsController

--- controllers/api/apiPatientsController.js
+++ controllers/api/apiPatientsController.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const passport = require('passport');
+const {Patient} = require('../../models/patients');
 
 const {labResultsSvc} = require('../../services/lab-results');
 const jwtAuth = passport.authenticate('jwt', {session: false});
@@ -13,8 +14,16 @@ router.get('/:patientId/lab-results', jwtAuth, function(req, res) {
     labResultsSvc.getAllLabResultsByPatientChronologically(req.params.patientId)
         .then(function(labResults) {
             res.json(labResults);
-        })
-})
+        });
+});
+
+router.get('/:patientId', jwtAuth, function(req, res) {
+    Patient
+        .findById(req.params.patientId)
+        .then(function(patient) {
+            res.json(patient);
+        });
+});
 
 module.exports = router;
 
